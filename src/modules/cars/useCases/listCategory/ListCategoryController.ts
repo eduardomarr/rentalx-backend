02d708c61497e142fe99062fe54d6321d95b4afc
eduardomarr <dado.nep@gmail.com>
2022-12-12 23:50:10 -0300

    feat: tsyringe applied

--- src/modules/cars/useCases/listCategory/ListCategoryController.ts
+++ src/modules/cars/useCases/listCategory/ListCategoryController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoryUseCase } from "./ListCategoryUseCase";
 
 class ListCategoryController {
-  constructor(private listCategoryUseCase: ListCategoryUseCase) {}
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listCategoryUseCase = container.resolve(ListCategoryUseCase);
 
-  handle(request: Request, response: Response): Response {
-    const all = this.listCategoryUseCase.execute();
+    const all = await listCategoryUseCase.execute();
 
     return response.json(all);
   }
