6f6b058eb643822bb7d231e0d9d38da94b764fe0
eduardomarr <dado.nep@gmail.com>
2022-12-16 13:56:35 -0300

    feat: user authentication

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,12 +1,9 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
 
 interface IUsersRepository {
-  create({
-    name,
-    email,
-    password,
-    driver_license,
-  }: ICreateUserDTO): Promise<void>;
+  create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
